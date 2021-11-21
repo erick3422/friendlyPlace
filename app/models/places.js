3f660cbc3721e53fe76226dd6fa5e38e0620e74b
erick3422 <>
2021-11-20 17:52:23 -0800

    2 Commit

---
+++ app/models/places.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose')
+
+const placesSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true
+    },
+    address: {
+      type: String,
+      required: true
+    },
+    location: {
+      type: String,
+      required: true
+    },
+    description: {
+      type: String,
+      required: true
+    },
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true
+    }
+  },
+  {
+    timestamps: true
+  }
+)
+
+module.exports = mongoose.model('places', placesSchema)
--- app/routes/places_routes.js
+++ app/routes/places_routes.js
@@ -4,13 +4,12 @@ const express = require('express')
 const passport = require('passport')
 
 // pull in Mongoose model for examples
-const Example = require('../models/example')
-
+const places = require('../models/places')
 // this is a collection of methods that help us detect situations when we need
 // to throw a custom error
 const customErrors = require('../../lib/custom_errors')
 
-// we'll use this function to send 404 when non-existant document is requested
+// we'll use this function to send 404 when non-existent document is requested
 const handle404 = customErrors.handle404
 // we'll use this function to send 401 when a user tries to modify a resource
 // that's owned by someone else
@@ -28,37 +27,37 @@ const requireToken = passport.authenticate('bearer', { session: false })
 const router = express.Router()
 
 // INDEX
-// GET /examples
-router.get('/examples', requireToken, (req, res, next) => {
-  Example.find()
+// GET / --- All Places
+router.get('/places', requireToken, (req, res, next) => {
+  places.find()
     // respond with status 200 and JSON of the examples
-    .then(examples => res.status(200).json({ examples: examples }))
+    .then(places => res.status(200).json({ places: places }))
     // if an error occurs, pass it to the handler
     .catch(next)
 })
 
 // SHOW
 // GET /examples/5a7db6c74d55bc51bdf39793
-router.get('/examples/:id', requireToken, (req, res, next) => {
+router.get('/places/:id', requireToken, (req, res, next) => {
   // req.params.id will be set based on the `:id` in the route
-  Example.findById(req.params.id)
+  places.findById(req.params.id)
     .then(handle404)
-    // if `findById` is succesful, respond with 200 and "example" JSON
-    .then(example => res.status(200).json({ example: example }))
+    // if `findById` is successful, respond with 200 and "example" JSON
+    .then(places => res.status(200).json({ places: places }))
     // if an error occurs, pass it to the handler
     .catch(next)
 })
 
 // CREATE
 // POST /examples
-router.post('/examples', requireToken, (req, res, next) => {
+router.post('/places', requireToken, (req, res, next) => {
   // set owner of new example to be current user
-  req.body.example.owner = req.user.id
+  req.body.places.owner = req.user.id
 
-  Example.create(req.body.example)
-    // respond to succesful `create` with status 201 and JSON of new "example"
-    .then(example => {
-      res.status(201).json({ example })
+  places.create(req.body.places)
+    // respond to successful `create` with status 201 and JSON of new "example"
+    .then(places => {
+      res.status(201).json({ places: places })
     })
     // if an error occurs, pass it off to our error handler
     // the error handler needs the error message and the `res` object so that it
@@ -68,17 +67,17 @@ router.post('/examples', requireToken, (req, res, next) => {
 
 // UPDATE
 // PATCH /examples/5a7db6c74d55bc51bdf39793
-router.patch('/examples/:id', requireToken, removeBlanks, (req, res, next) => {
+router.patch('/places/:id', requireToken, removeBlanks, (req, res, next) => {
   // if the client attempts to change the `owner` property by including a new
   // owner, prevent that by deleting that key/value pair
-  delete req.body.example.owner
+  delete req.body.places.owner
 
-  Example.findById(req.params.id)
+  places.findById(req.params.id)
     .then(handle404)
     // ensure the signed in user (req.user.id) is the same as the example's owner (example.owner)
-    .then(example => requireOwnership(req, example))
+    .then(places => requireOwnership(req, places))
     // updating example object with exampleData
-    .then(example => example.updateOne(req.body.example))
+    .then(places => places.updateOne(req.body.places))
     // if that succeeded, return 204 and no JSON
     .then(() => res.sendStatus(204))
     // if an error occurs, pass it to the handler
@@ -87,13 +86,13 @@ router.patch('/examples/:id', requireToken, removeBlanks, (req, res, next) => {
 
 // DESTROY
 // DELETE /examples/5a7db6c74d55bc51bdf39793
-router.delete('/examples/:id', requireToken, (req, res, next) => {
-  Example.findById(req.params.id)
+router.delete('/places/:id', requireToken, (req, res, next) => {
+  places.findById(req.params.id)
     .then(handle404)
-     // ensure the signed in user (req.user.id) is the same as the example's owner (example.owner)
-    .then(example => requireOwnership(req, example))
+  // ensure the signed in user (req.user.id) is the same as the example's owner (example.owner)
+    .then(places => requireOwnership(req, places))
     // delete example from mongodb
-    .then(example => example.deleteOne())
+    .then(places => places.deleteOne())
     // send back 204 and no content if the deletion succeeded
     .then(() => res.sendStatus(204))
     // if an error occurs, pass it to the handler
